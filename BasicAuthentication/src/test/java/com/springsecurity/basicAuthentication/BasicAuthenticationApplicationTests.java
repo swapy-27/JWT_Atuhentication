6dcc3841b8236ef09cef3aa09212a3440dea7ffd
Swapnil Jain <swapniljain240@gmail.com>
2023-07-02 01:28:36 +0530

    code restructure

---
+++ BasicAuthentication/src/test/java/com/springsecurity/basicAuthentication/BasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.basicAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
